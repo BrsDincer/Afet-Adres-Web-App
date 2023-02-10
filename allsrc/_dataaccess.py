20012099db839759c0854ebcb2648cbc9dbddaa5
Baris Dincer (B') <81470496+BrsDincer@users.noreply.github.com>
2023-02-11 01:14:30 +0300

    Update _dataaccess.py

--- allsrc/_dataaccess.py
+++ allsrc/_dataaccess.py
@@ -9,6 +9,18 @@ class DATAGATHERING(object):
         #"https://afetadres.s3.eu-central-1.amazonaws.com/gas_station.csv"
         #"https://afetadres.s3.eu-central-1.amazonaws.com/crane_operator.csv"
         #"https://afetadres.s3.eu-central-1.amazonaws.com/internet_scan.csv"
+        #"https://afetadres.s3.eu-central-1.amazonaws.com/child_need_data.csv"
+        #"https://afetadres.s3.eu-central-1.amazonaws.com/debris_need_data.csv"
+        #"https://afetadres.s3.eu-central-1.amazonaws.com/provision_need_data.csv"
+        #"https://afetadres.s3.eu-central-1.amazonaws.com/injury_need_data.csv"
+        #"https://afetadres.s3.eu-central-1.amazonaws.com/mersin_konaklama.csv"
+        #"https://afetadres.s3.eu-central-1.amazonaws.com/ShelterandAccommodationAreas.csv"
+        #"https://afetadres.s3.eu-central-1.amazonaws.com/FoodSpots.csv"
+        #"https://afetadres.s3.eu-central-1.amazonaws.com/TransportationToDisasterAreas.csv"
+        #"https://afetadres.s3.eu-central-1.amazonaws.com/ShelterOpportunitiesOutsideTheDisasterArea.csv"
+        #"https://afetadres.s3.eu-central-1.amazonaws.com/PharmacyTrucks.csv"
+        #"https://afetadres.s3.eu-central-1.amazonaws.com/LocationsofAidTrucks.csv"
+        #"https://afetadres.s3.eu-central-1.amazonaws.com/AuthorizedContactNumbers.csv"
     """
     def __init__(self,dm:str):
         self.__dt = dm
